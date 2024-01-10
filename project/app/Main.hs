cd71399d1159abce9ef71a44f5641c5e2e7c0e56
Christophe <ch.alexandre@bluewin.ch>
2024-01-10 18:20:04 +0000

    dev

--- project/app/Main.hs
+++ project/app/Main.hs
@@ -4,15 +4,14 @@
 module Main (main) where
 
 import Aws.Lambda
-import Lib
+import qualified Data.Text as T
 import qualified Lib
 
--- This tells the Lambda runtime how to initialize your application context.
--- If you do not wish to use a shared context, you can just use Unit as the context value.
--- E.g.
--- initializeContext :: IO ()
--- initializeContext = return ()
-initializeContext :: IO AppConfig
-initializeContext = Lib.initializeAppConfig
 
-generateLambdaDispatcher UseWithAPIGateway defaultDispatcherOptions
+main :: IO ()
+main =
+  runLambdaHaskellRuntime
+    defaultDispatcherOptions
+    (pure ())
+    id
+    (addStandaloneLambdaHandler (HandlerName (T.pack "handler")) Lib.handler)

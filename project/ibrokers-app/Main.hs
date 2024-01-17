cdd7564bc5d74ede2aead2cddeb724e0f334697a
Christophe <chris-ch@users.noreply.github.com>
2024-01-17 21:29:36 +0000

    dev

--- project/ibrokers-app/Main.hs
+++ project/ibrokers-app/Main.hs
@@ -1,9 +1,22 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main (main) where
 
 import System.Environment ( lookupEnv )
 
 import IBrokersReports (loadIBRecords, checkReportError, makeIBRecords)
-import Text.Printf ( printf )
+import Control.Exception (Exception, throw)
+import Data.Data (Typeable)
+import Data.Text (Text, pack)
+
+data IBrokersException
+  = LoadingFailed !Text
+  | ReportServerError !Text
+  | EnvironmentVariableMissing !Text
+  deriving (Show, Typeable)
+
+instance Exception IBrokersException
+
 
 main :: IO ()
 main = do
@@ -14,7 +27,10 @@ main = do
             maybeTree <- loadIBRecords token qId
             case maybeTree of
                 Just tree -> case checkReportError tree of
-                    Just (errorCode, errorMessage) -> printf "Error: failed to load data: %s (code %d)\n" errorMessage errorCode
+                    Just (errorCode, errorMessage) -> throw (ReportServerError (pack msg))
+                        where msg = "report not ready: " ++ errorMessage ++ " (code " ++ show errorCode ++ ")"
                     Nothing -> print $ makeIBRecords tree
-                Nothing -> putStrLn "failed to load data"
-        _ -> putStrLn "required environment variables: IB_FLEX_QUERY_ID and IB_FLEX_REPORT_TOKEN"
+                Nothing -> throw (LoadingFailed "failed to load data")
+        (Just _, Nothing) -> throw (EnvironmentVariableMissing "required environment variable IB_FLEX_REPORT_TOKEN is missing")
+        (Nothing, Just _) -> throw (EnvironmentVariableMissing "required environment variable IB_FLEX_QUERY_ID is missing")
+        (Nothing, Nothing) -> throw (EnvironmentVariableMissing "required environment variables IB_FLEX_QUERY_ID and IB_FLEX_REPORT_TOKEN are missing")

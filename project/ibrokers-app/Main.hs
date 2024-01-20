5b70d4628f1c58aec0b0bd4e6b6b72f04a218480
Christophe <chris-ch@users.noreply.github.com>
2024-01-20 17:05:32 +0000

    dev

--- project/ibrokers-app/Main.hs
+++ project/ibrokers-app/Main.hs
@@ -5,7 +5,7 @@
 
 module Main (main) where
 
-import System.Environment (lookupEnv, getEnv)
+import System.Environment (lookupEnv)
 import IBrokersReports (fetchFlexReport, makeIBRecords)
 import Data.Maybe (fromMaybe)
 
@@ -16,36 +16,28 @@ import Data.Aeson.Types (Value, parseMaybe)
 import Data.Text (unpack)
 import Data.Text.Lazy (toStrict)
 
-data FlexReportResult = FlexReportResult { message :: String } deriving Generic
+data FlexReportResult where
+  FlexReportResult :: {message :: String} -> FlexReportResult
+  deriving Generic
 instance ToJSON FlexReportResult
 
-data FlexReportEvent = FlexReportEvent {
-    flexQueryId :: String
-} deriving Generic
+data FlexReportEvent where
+  FlexReportEvent :: {flexQueryId :: String} -> FlexReportEvent
+  deriving Generic
 instance FromJSON FlexReportEvent
 
-handler :: Value -> IO String
+handler :: Value -> IO FlexReportResult
 handler jsonAst =
     case parseMaybe parseJSON jsonAst of
-        Nothing -> return "Missing flexQueryId"
+        Nothing -> return FlexReportResult { message = "Missing flexQueryId" }
         Just FlexReportEvent { flexQueryId } -> do
             flexReportToken <- lookupEnv "IB_FLEX_REPORT_TOKEN"
             ibFlexURLEnv <- lookupEnv "IB_FLEX_URL"
             let ibFlexURL = fromMaybe ibFlexUrlDefault ibFlexURLEnv
             reportTree <- fetchFlexReport ibFlexURL flexQueryId flexReportToken
-            return $ (unpack . toStrict . makeIBRecords) reportTree
+            return $ FlexReportResult { message = (unpack . toStrict . makeIBRecords) reportTree }
     where
         ibFlexUrlDefault = "https://www.interactivebrokers.com/Universal/servlet/FlexStatementService.SendRequest"
 
 main :: IO ()
 main = mRuntime handler
-
-main' :: IO ()
-main' = do
-    let ibFlexUrlDefault = "https://www.interactivebrokers.com/Universal/servlet/FlexStatementService.SendRequest"
-    flexQueryId <- getEnv "IB_FLEX_QUERY_ID"
-    flexReportToken <- lookupEnv "IB_FLEX_REPORT_TOKEN"
-    ibFlexURLEnv <- lookupEnv "IB_FLEX_URL"
-    let ibFlexURL = fromMaybe ibFlexUrlDefault ibFlexURLEnv
-    reportTree <- fetchFlexReport ibFlexURL flexQueryId flexReportToken
-    print $ makeIBRecords reportTree

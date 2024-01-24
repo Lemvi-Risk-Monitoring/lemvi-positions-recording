373ef63da48fa6f01053ec8ad352b6cf7a9ec9b4
Christophe <chris-ch@users.noreply.github.com>
2024-01-24 12:01:50 +0000

    dev

--- project/src/DeribitReports.hs
+++ project/src/DeribitReports.hs
@@ -5,28 +5,49 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE DeriveAnyClass #-}
+
 
 module DeribitReports (app) where
 
 import qualified Data.ByteString as BS
 import qualified Data.ByteString.Char8 as BSC
+import qualified Data.Text as T
+import qualified Data.Aeson as A
+import qualified Data.Aeson.KeyMap as KM
+import qualified Data.Text.Encoding as TE
 
-import GHC.Generics       (Generic)
-import Network.HTTP.Simple (parseRequest, getResponseBody, httpBS, setRequestHeaders)
-import Data.Aeson ( Value, decodeStrict, ToJSON, withObject, (.:) )
+import GHC.Generics (Generic)
+import Network.HTTP.Simple (parseRequest, getResponseBody, httpBS, setRequestHeaders, getResponseStatus, setRequestQueryString)
+import Data.Aeson ( ToJSON, withObject, (.:) )
 import Data.Aeson.Types ( parseMaybe, Parser )
 import System.Environment (lookupEnv)
 import Servant (Proxy(Proxy), (:>), JSON, Post, ReqBody)
 import Servant.Server (Application, serve, Handler)
 import Control.Monad.IO.Class (liftIO)
-import Network.HTTP.Client (setQueryString)
 import Data.String (IsString)
+import Network.HTTP.Types.Status (statusIsSuccessful)
+import qualified Amazonka as AWS
+import qualified Amazonka.S3 as S3
 
 newtype DeribitClientId = DeribitClientId {getClientId :: BS.ByteString} deriving newtype (IsString, Show)
 newtype DeribitClientSecret = DeribitClientSecret {getClientSecret ::BS.ByteString} deriving newtype (IsString, Show)
 
-authorizeWithCredentials :: DeribitClientId -> DeribitClientSecret -> String -> IO (Maybe (String, String))
-authorizeWithCredentials deribitClientId deribitClientSecret publicURL =  do
+data TokenResponse = TokenResponse
+  {
+    accessToken :: String,
+    refreshToken :: String
+  } deriving (Show)
+
+tokenParser :: A.Value -> Parser TokenResponse
+tokenParser = withObject "AccessTokenResponse" $ \obj -> do
+  resultObj <- obj .: "result"
+  TokenResponse
+    <$> resultObj .: "access_token"
+    <*> resultObj .: "refresh_token"
+
+authorizeWithCredentials :: DeribitClientId -> DeribitClientSecret -> String -> IO (Maybe (BS.ByteString, BS.ByteString))
+authorizeWithCredentials deribitClientId deribitClientSecret publicURL = do
     request <- parseRequest $ publicURL <> "/auth"
     let
         headers = [("Content-Type", "application/json")]
@@ -36,32 +57,19 @@ authorizeWithCredentials deribitClientId deribitClientSecret publicURL =  do
             ("client_secret", Just (getClientSecret deribitClientSecret)),
             ("grant_type", Just "client_credentials")
           ]
-        withAuthQuery = setQueryString queryParams withHeaders
+        withAuthQuery = setRequestQueryString queryParams withHeaders
     ibResponse <- httpBS withAuthQuery
-    let maybeAccessToken = parseMaybe tokenParser =<< decodeStrict (getResponseBody ibResponse)
+    let maybeAccessToken = parseMaybe tokenParser =<< A.decodeStrict (getResponseBody ibResponse)
     case maybeAccessToken of
-      Just tokenResponse -> return $ Just (accessToken tokenResponse, refreshToken tokenResponse)
+      Just tokenResponse -> return $ Just (BSC.pack $ accessToken tokenResponse, BSC.pack $ refreshToken tokenResponse)
       Nothing -> return Nothing
 
-data TokenResponse = TokenResponse
-  {
-    accessToken :: String,
-    refreshToken :: String
-  } deriving Show
+data DeribitReportResult =
+  ReportContent T.Text
+  | ReportError T.Text
+  deriving (Generic, ToJSON)
 
-tokenParser :: Value -> Parser TokenResponse
-tokenParser = withObject "AccessTokenResponse" $ \obj -> do
-  resultObj <- obj .: "result"
-  TokenResponse
-    <$> resultObj .: "access_token"
-    <*> resultObj .: "refresh_token"
-
-data DeribitReportResult where
-  DeribitReportResult :: {message :: String} -> DeribitReportResult
-  deriving Generic
-instance ToJSON DeribitReportResult
-
-handler :: Value -> IO DeribitReportResult
+handler :: A.Value -> IO DeribitReportResult
 handler _ = do
             deribitClientId <- lookupEnv "DERIBIT_CLIENT_ID"
             deribitClientSecret <- lookupEnv "DERIBIT_CLIENT_SECRET"
@@ -69,9 +77,9 @@ handler _ = do
                 (Just deribitClientId', Just deribitClientSecret') -> do
                   credentials <- authorizeWithCredentials (DeribitClientId (BSC.pack deribitClientId')) (DeribitClientSecret (BSC.pack deribitClientSecret')) publicURL
                   case credentials of
-                    Nothing ->  return $ DeribitReportResult { message = "authentication failed" }
-                    Just (accessToken, refreshToken) -> return $ DeribitReportResult { message = "successfully authentified" }
-                _ -> return $ DeribitReportResult { message = "error: environment variables DERIBIT_CLIENT_ID and DERIBIT_CLIENT_SECRET are required" }
+                    Nothing ->  return $ ReportError "authentication failed"
+                    Just (token, _) -> savePositions token privateURL "BTC"
+                _ -> return $ ReportError "error: environment variables DERIBIT_CLIENT_ID and DERIBIT_CLIENT_SECRET are required"
 
     where
         hostName = "www.deribit.com"
@@ -79,13 +87,49 @@ handler _ = do
         publicURL = deribitEndpointV2 <> "/public"
         privateURL = deribitEndpointV2 <> "/private"
 
+savePositions :: BS.ByteString -> String -> BS.ByteString -> IO DeribitReportResult
+savePositions token privateURL currencyCode = do
+    request <- parseRequest $ privateURL <> "/get_positions"
+    let
+        headers = [
+          ("Content-Type", "application/json"),
+          ("Authorization", "Bearer " <> token)
+          ]
+        withHeaders = setRequestHeaders headers request
 
-handlers :: Value -> Handler DeribitReportResult
+        queryParams = [
+            ("currency", Just currencyCode)
+          ]
+        withBodyParams = setRequestQueryString queryParams withHeaders
+    ibResponse <- httpBS withBodyParams
+    let
+      responseBody = getResponseBody ibResponse
+      responseStatus = getResponseStatus ibResponse
+      success = statusIsSuccessful responseStatus
+    if success then
+        case A.decodeStrict responseBody :: Maybe (A.Value) of
+          Just (A.Object json) -> (if KM.member "result" json then (do
+                  writeToS3 (TE.decodeUtf8 responseBody)
+                  return $ ReportContent (TE.decodeUtf8 responseBody))
+                else
+                  return $ ReportError $ "no result found in body: " <> TE.decodeUtf8 responseBody)
+          _ -> return $ ReportError $ "failed to parse body: " <> TE.decodeUtf8 responseBody        
+      else
+      return $ ReportError $ "failed to load positions: " <> TE.decodeUtf8 responseBody
+
+writeToS3 :: T.Text -> IO ()
+writeToS3 json = do
+  env <- AWS.newEnv AWS.discover
+  resp <- AWS.runResourceT $ AWS.send env S3.newListBuckets
+  print $ resp
+  return ()
+
+handlers :: A.Value -> Handler DeribitReportResult
 handlers = liftIO . handler
 
 type Api =
   "endpoint"
-    :> ReqBody '[JSON] Value
+    :> ReqBody '[JSON] A.Value
     :> Post '[JSON] DeribitReportResult
 
 app :: Application
